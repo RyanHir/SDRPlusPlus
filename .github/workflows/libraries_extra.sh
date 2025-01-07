f0b8f473afd9fd5b393f9ca0b8ce78a7d1b186c8
Ryan Hirasaki <ryanhirasaki@gmail.com>
2025-01-06 22:04:32 -0600

    Build debian packages with extras

---
+++ .github/workflows/libraries_extra.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+set -e
+cd /root
+
+# Install SDRPlay libraries
+wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.15.1.run
+7z x ./SDRplay_RSP_API-Linux-3.15.1.run
+7z x ./SDRplay_RSP_API-Linux-3.15.1
+cp x86_64/libsdrplay_api.so.3.15 /usr/lib/libsdrplay_api.so
+cp inc/* /usr/include/
+
+# Install libperseus
+git clone https://github.com/Microtelecom/libperseus-sdr
+cd libperseus-sdr
+autoreconf -i
+./configure
+make
+make install
+ldconfig
+cd ..
+
+# Install librfnm
+git clone https://github.com/AlexandreRouma/librfnm
+cd librfnm
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+make -j2
+make install
+cd ../../
+
+# Install libfobos
+git clone https://github.com/AlexandreRouma/libfobos
+cd libfobos
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+make -j2
+make install
+cd ../../
