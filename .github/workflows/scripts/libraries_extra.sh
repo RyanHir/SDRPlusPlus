34567f122d529b2027997df33fd6cfbdc2ed6c8d
Ryan Hirasaki <ryanhirasaki@gmail.com>
2025-01-07 01:21:29 -0600

    Conditionally enable sdrplay support on x86_64 and aarch64 targets

--- .github/workflows/scripts/libraries_extra.sh
+++ .github/workflows/scripts/libraries_extra.sh
@@ -2,12 +2,19 @@
 set -e
 cd /root
 
+ARCH="$(uname -m)"
+
 # Install SDRPlay libraries
 wget --no-check-certificate https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.15.1.run
 7z x ./SDRplay_RSP_API-Linux-3.15.1.run
 7z x ./SDRplay_RSP_API-Linux-3.15.1
-cp x86_64/libsdrplay_api.so.3.15 /usr/lib/libsdrplay_api.so
-cp inc/* /usr/include/
+if [ "$ARCH" = x86_64 ]; then
+    cp x86_64/libsdrplay_api.so.3.15 /usr/lib/libsdrplay_api.so
+    cp inc/* /usr/include/
+else if [ "$ARCH" = aarch64 ]; then
+    cp aarch64/libsdrplay_api.so.3.15 /usr/lib/libsdrplay_api.so
+    cp inc/* /usr/include/
+fi
 
 # Install libperseus
 git clone https://github.com/Microtelecom/libperseus-sdr
